52cab429f216505efa63cbfa6ebc0d978d0639e1
tucker & Dex Fitch <student@epicodus.com>
2015-01-22 16:56:59 -0800

    add more frontend work

--- app.rb
+++ app.rb
@@ -13,6 +13,7 @@ end
 
 get("/categories/:id") do
   @id = params["id"].to_i()
+
   erb(:categories)
 end
 
@@ -27,13 +28,38 @@ post("/categories") do
     new_category = Category.new({:category_name => category_name})
     new_category.save()
   end
-  erb(:)
+  erb(:index)
 end
 
 post("/expenses") do
-  description = param['description'].strip()
-  cost = param['cost'].strip()
-  date = param['date'].strip()
+  description = params['description'].strip()
+  cost = params['cost'].strip()
+  date = params['date'].strip()
+  if description != "" && cost != "" && date != ""
+    new_expense = Expense.new({:description => description, :cost => cost, :date => date})
+    new_expense.save()
   end
-  erb(:)
+  erb(:index)
+end
+
+post("/categories/:id") do
+  id = params['id'].to_i()
+  associated_expense = Expense.find(id)
+
+  erb(:categories)
+end
+#
+# post("/lines/:line_id") do
+#   line_id = params.fetch('line_id').to_i()
+#   station_id = params.fetch('station_id').to_i()
+#   line = Line.find(line_id)
+#   station = Station.find(station_id)
+#   station.add_line(line)
+#   url = "/admin/lines/" + line_id.to_s()
+#   redirect(url)
+# end
+
+post("/expenses/:id") do
+
+  erb(:expenses)
 end
